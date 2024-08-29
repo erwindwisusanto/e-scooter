96055f75d6a118b93ebcb3c5bdc34ef503805850
erwin dwi s <erwin@waki.mobi>
2024-08-29 11:52:45 +0700

    feat: remove loader

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Cache::remember('index', 60, function () {
-        return view('index')->render();
-    });
+    return view('index');
 });
